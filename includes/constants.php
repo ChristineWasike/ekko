7fdcdef6fd724a3d378b8b0fd2ecf87f4301671a
ChristineWasike <christinewasike22@gmail.com>
2020-05-12 10:06:17 +0300

    Changed localhost name to 127.0.0.1 in the includes folder

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 
 // Defining all the constants that will be sued in the application
 
-define("DATABASE_HOST", "localhost");
+define("DATABASE_HOST", "127.0.0.1");
 define("DATABASE_USERNAME", "root");
 define("DATABASE_NAME", "ekko");
 define("DATABASE_PASSWORD", "");
